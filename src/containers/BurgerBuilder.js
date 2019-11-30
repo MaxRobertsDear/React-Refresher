cb431c55bcb3d209a8c3d3fecb1ee0f1874b8873
MaxRobertsDear <maxhroberts@gmail.com>
2019-11-30 22:06:00 +0000

    Added modal and backdrop modal shows order summary and backdrop appears when modal appears

--- src/containers/BurgerBuilder.js
+++ src/containers/BurgerBuilder.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import Burger from '../components/Burger/Burger'
 import BuildControls from '../components/Burger/BuildControls/BuildControls'
+import Modal from '../components/UI/Modal/Modal'
+import OrderSummary from '../components/Burger/OrderSummary/OrderSummary'
 
 const INGREDIENT_PRICES = {
     salad: 0.5, 
@@ -18,7 +20,31 @@ class BurgerBuilder extends Component {
             cheese: 0, 
             meat: 0, 
         }, 
-        totalPrice: 4.0
+        totalPrice: 4.0, 
+        purchaseable: false,
+        purchasing: false,
+    }
+
+    purchaseHandler = () => {
+        this.setState({purchasing: true})
+    }
+
+    purchaseCancelHandler = () => {
+        this.setState({purchasing: false})
+    }
+
+    updatePurchaseable(ingredients) {
+        const sum = Object.keys(ingredients)
+            .map(igKey => {
+                return ingredients[igKey]
+            })
+            .reduce((sum, el) => {
+                console.log(`sum ${sum}`)
+                console.log(`el ${el}`)
+                return sum + el
+            }, [])
+
+            this.setState({purchaseable: sum>0})
     }
 
     addIngredientHandler = (type) => {
@@ -32,8 +58,9 @@ class BurgerBuilder extends Component {
         const oldPrice = this.state.totalPrice
         const newPrice = oldPrice + priceAddition
         this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
+        this.updatePurchaseable(updatedIngredients)
     }
-
+    
     removeIngredientHandler = (type) => {
         const oldCount = this.state.ingredients[type]
         if (oldCount <= 0) {
@@ -48,6 +75,7 @@ class BurgerBuilder extends Component {
         const oldPrice = this.state.totalPrice
         const newPrice = oldPrice - priceAddition
         this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
+        this.updatePurchaseable(updatedIngredients)
     }
     
     render() {
@@ -60,11 +88,17 @@ class BurgerBuilder extends Component {
 
         return (
             <>
+                <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
+                    <OrderSummary ingredients={this.state.ingredients}/>
+                </Modal>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls 
                     ingredientAdded={this.addIngredientHandler} 
                     ingredientRemoved={this.removeIngredientHandler}
-                    disabled={disabledInfo} />
+                    disabled={disabledInfo}
+                    price={this.state.totalPrice}
+                    purchaseable={this.state.purchaseable}
+                    ordered={this.purchaseHandler} />
             </>
         )
     }
